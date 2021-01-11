7a3d3ce876dca988f108eb3cde3f31306b0d364a
EstagioAutomacao <estagiodaautomacao@gmail.com>
2021-01-10 22:47:26 -0300

    Firebase

--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -1,6 +1,22 @@
 var CACHE_NAME = 'react-pwa';
 var urlsToCache = [
   '/',
+  '/index.html',
+  '/404.html',
+  '/favicon.ico',
+  '/register-sw.js',
+  '/service-worker.js',
+  '/asset-manifest.json',
+  '/manifest.json',
+  '/maskable_icon_x1_512.png',
+  '/maskable_icon_x1_192.png',
+  '/static/css/main.96cbabe0.chunk.css',
+  '/static/media/heroes.b413d9df.png',
+  '/static/media/logo.4f957741.svg',
+  '/static/js/2.27c3e997.chunk.js',
+  '/static/js/3.13c9e47b.chunk.js',
+  '/static/js/main.0ccbac42.chunk.js',
+  '/static/js/runtime-main.def9cf0b.js',
 ];
 
 // self.addEventListener('install', (event) => {
